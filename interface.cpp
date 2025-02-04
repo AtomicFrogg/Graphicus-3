79dc68c853d1fb4679b592b13f438ae12b0e3011
BipboupBoum <lapw3471@usherbrooke.ca>
2025-02-04 10:28:06 -0500

    création d'un affichage

---
+++ interface.cpp
@@ -0,0 +1,38 @@
+#include <random>
+#include <sstream>
+#include "Interface.h"
+
+using namespace std;
+
+Interface::Interface(const char* theName) : GraphicusGUI(theName)
+{
+	reinitialiserCanevas();
+	effacerInformations();
+}
+
+void Interface::reinitialiserCanevas()
+{
+	ostringstream os;
+	random_device r;
+	default_random_engine generator(r());
+	uniform_int_distribution<int> coor(0, 300), dim(20, 100);
+
+	// On s'amuse à générer aléatoirement un canevas en format texte à chaque
+	// fois que la commande de réinitialisation de canevas est choisie par l'usager.
+	os << "L x" << endl;
+	os << "R " << coor(generator) << " " << coor(generator) << " " << dim(generator) << " " << dim(generator) << endl;
+	os << "K " << coor(generator) << " " << coor(generator) << " " << dim(generator) << endl;
+	os << "L a" << endl;
+	os << "K " << coor(generator) << " " << coor(generator) << " " << dim(generator) << endl;
+	os << "C " << coor(generator) << " " << coor(generator) << " " << dim(generator) << endl;
+	os << "L x" << endl;
+	os << "R " << coor(generator) << " " << coor(generator) << " " << dim(generator) << " " << dim(generator) << endl;
+	os << "C " << coor(generator) << " " << coor(generator) << " " << dim(generator) << endl;
+	os << "L x" << endl;
+	os << "R " << coor(generator) << " " << coor(generator) << " " << dim(generator) << " " << dim(generator) << endl;
+	os << "K " << coor(generator) << " " << coor(generator) << " " << dim(generator) << endl;
+	os << "C " << coor(generator) << " " << coor(generator) << " " << dim(generator) << endl;
+
+	// Ensuite, on dessine ce qui a été généré dans Graphicus
+	dessiner(os.str().c_str());
+}
---
+++ interface.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "graphicusGUI.h"
+#include "canevas.h"
+
+class Interface : public GraphicusGUI
+{
+private:
+	Canevas dessin;
+public:
+	Interface(const char* theName = nullptr);
+	virtual bool ouvrirFichier(const char*);
+	virtual bool sauvegarderFichier(const char*);
+	virtual void coucheTranslater(int deltaX, int deltaY);
+	virtual void ajouterCercle(int x, int y, int rayon);
+	virtual void ajouterRectangle(int x, int y, int long_x, int long_y);
+	virtual void ajouterCarre(int x, int y, int cote);
+	virtual void modePileChange(bool mode);
+
+	// Menu Canevas
+	virtual void reinitialiserCanevas();
+	virtual void coucheAjouter();
+	virtual void coucheRetirer();
+	// Menu Formes
+	virtual void retirerForme();
+	// Menu navigation
+	virtual void couchePremiere();
+	virtual void couchePrecedente();
+	virtual void coucheSuivante();
+	virtual void coucheDerniere();
+	virtual void formePremiere();
+	virtual void formePrecedente();
+	virtual void formeSuivante();
+	virtual void formeDerniere();
+};
