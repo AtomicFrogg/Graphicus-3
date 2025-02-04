16589dc624b355ccdf1d53e2fe45b5e71b08117c
BipboupBoum <lapw3471@usherbrooke.ca>
2025-02-04 09:44:10 -0500

    BOOOOOOO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- vecteur.h
+++ vecteur.h
@@ -19,62 +19,70 @@ public :
 	void afficher(ostream &s);
 	T retirerElement(int index);
 	T getElement(int index);
+	T operator[](int i);
+	void operator+=(T item);
+	/*T operator>>();
+	T operator<<();*/
+	T operator++();
+	T operator--();
 	
 
 private:
 	int capacite;
 	int taille;
+	int position;
 	T *elements;
 };
 
 template <class T>
 Vecteur<T>::Vecteur() {
-	this->capacite = 1;
-	this->taille = 0;
-	this->elements = new T[this->capacite];
+	capacite = 1;
+	taille = 0;
+	position = 0;
+	elements = new T[capacite];
 }
 
 
 template <class T>
 Vecteur<T>::~Vecteur() {
-	delete[] this->elements;
+	delete[] elements;
 }
 
 template <class T>
 int Vecteur<T>::getCapacite() {
-	return this->capacite;
+	return capacite;
 }
 
 template <class T>
 int Vecteur<T>::getTaille() {
-	return this->taille;
+	return taille;
 }
 
 template <class T>
 void Vecteur<T>::doublerCapacite() {
-	this->capacite = this->capacite * 2;
+	capacite = capacite * 2;
 	T* elements2;
-	elements2 = new T[this->capacite];
+	elements2 = new T[capacite];
 	//Copier le contenu
-	for (int i = 0; i < this->taille; i++) {
-		elements2[i] = this->elements[i];
+	for (int i = 0; i < taille; i++) {
+		elements2[i] = elements[i];
 	}
-	delete[] this->elements;
-	this->elements = elements2;
+	delete[] elements;
+	elements = elements2;
 }
 
 template <class T>
 void Vecteur<T>::viderVecteur() {
 	//Supprimer l'ancien tableau pour en cr�er un nouveau vide
-	delete[] this->elements;
-	this->elements = new T[this->capacite];
-	this->taille = 0;
+	delete[] elements;
+	elements = new T[capacite];
+	taille = 0;
 }
 
 
 template <class T>
 bool Vecteur<T>::estVide() {
-	return this->taille == 0;
+	return taille == 0;
 
 }
 
@@ -82,12 +90,12 @@ bool Vecteur<T>::estVide() {
 template <class T>
 bool Vecteur<T>::ajouterElement(T element) {
 	//V�rifier si plein
-	if (this->taille >= this->capacite) {
-		this->doublerCapacite();
+	if (taille >= capacite) {
+		doublerCapacite();
 	}
 	//Ajouter le nouvel �l�ment � la fin
-	this->elements[this->taille] = element;
-	this->taille++;
+	elements[taille] = element;
+	taille++;
 	return true;
 }
 
@@ -108,13 +116,13 @@ void Vecteur::afficher(ostream &s){
 
 template <class T>
 void Vecteur<T>::afficher(ostream& s) {
-	if (this->taille == 0) {
+	if (taille == 0) {
 		s << "Vecteur vide" << endl;
 	}
 	else {
 		for (int i = 0; i < taille; i++) {
 
-			this->elements[i]->afficher(s);
+			elements[i]->afficher(s);
 		}
 	}
 
@@ -124,13 +132,13 @@ void Vecteur<T>::afficher(ostream& s) {
 
 template <class T>
 T Vecteur<T>::retirerElement(int index) {
-	if (index < this->taille && index >= 0) {
-		T elementRetire = this->elements[index];
+	if (index < taille && index >= 0) {
+		T elementRetire = elements[index];
 		//Tasser tous ceux apr�s
-		for (int i = index; i < this->taille - 1; i++) {
-			this->elements[i] = this->elements[i + 1];
+		for (int i = index; i < taille - 1; i++) {
+			elements[i] = elements[i + 1];
 		}
-		this->taille--;
+		taille--;
 		return elementRetire;
 	}
 	else {
@@ -142,12 +150,52 @@ T Vecteur<T>::retirerElement(int index) {
 template <class T>
 T Vecteur<T>::getElement(int index) {
 
-	if (index < this->taille && index >= 0) {
-		return this->elements[index];
+	if (index < taille && index >= 0) {
+		return elements[index];
 	}
 	else {
 		return nullptr;
 	}
+}
+
+template<class T>
+void Vecteur<T>::operator+=(T item)
+{
+	ajouterElement(item);
+}
+
+/*template<class T>
+T Vecteur<T>::operator>>()
+{
+	return T();
+}
+
+template<class T>
+T Vecteur<T>::operator<<()
+{
+	return T();
+}*/
+
+template<class T>
+T Vecteur<T>::operator++()
+{
+	++position;
+	return elements[position%taille];
+}
+
+template<class T>
+T Vecteur<T>::operator--()
+{
+	if (!position)
+		return elements[taille];
+	--position;
+	return elements[position];
+}
+
+template<class T>
+T Vecteur<T>::operator[](int i )
+{
+	return elements[i];
 }
 
 
